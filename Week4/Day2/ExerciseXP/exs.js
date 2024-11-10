ef7de3bf02ffb858954bd432b6c76c2923159c5a
DanielOlivo <vit.masterov@gmail.com>
2024-11-10 14:19:57 +0200

    week 4 day 2 exercise 1

---
+++ Week4/Day2/ExerciseXP/exs.js
@@ -0,0 +1,53 @@
+// exercise 1: list of people
+let people = ["Greg", "Mary", "Devon", "James"];
+people = ["Greg", "Mary", "Devon", "James"];
+
+// Part I - Review about arrays
+// Write code to remove “Greg” from the people array.
+people.splice(0,1);
+people
+
+// Write code to replace “James” to “Jason”.
+people[2] = "Jason";
+people
+
+// Write code to add your name to the end of the people array.
+people.push("Vitalii")
+people
+
+
+// Write code that console.logs Mary’s index. take a look at the indexOf method on Google.
+console.log(people.indexOf("Mary"));
+
+// Write code to make a copy of the people array using the slice method.
+// The copy should NOT include “Mary” or your name.
+// Hint: remember that now the people array should look like this const people = ["Mary", "Devon", "Jason", "Yourname"];
+// Hint: Check out the documentation for the slice method
+let people_copy = people.slice(1,3);
+people_copy = people.slice(1,3);
+people_copy
+
+// Write code that gives the index of “Foo”. Why does it return -1 ?
+console.log(people_copy.indexOf("Foo"));
+// because it is not in the array
+
+// Create a variable called last which value is the last element of the array.
+// Hint: What is the relationship between the index of the last element in the array and the length of the array?
+let last = people_copy.slice(-1);
+last
+
+
+// Part II - Loops
+// Using a loop, iterate through the people array and console.log each person.
+for(i = 0; i < people.length; i++){
+    console.log(people[i]);
+}
+
+// Using a loop, iterate through the people array and exit the loop after you console.log “Devon” .
+// Hint: take a look at the break statement in the lesson.
+for(i = 0; i < people.length; i++){
+    console.log(people[i]);
+    if (people[i] == "Devon"){
+        break;
+    }
+}
