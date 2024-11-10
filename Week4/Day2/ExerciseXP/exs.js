90a39d7bd03e2f19df1352ddb96f67f22fe7374d
DanielOlivo <vit.masterov@gmail.com>
2024-11-10 15:48:12 +0200

    week 4 day 2 exercise 4

--- Week4/Day2/ExerciseXP/exs.js
+++ Week4/Day2/ExerciseXP/exs.js
@@ -93,3 +93,49 @@ let suffixes = ["st", "nd", "rd"];
 //         break;
 //     }
 // }
+
+
+// exercise 4: building management
+// Copy and paste the above object to your Javascript file.
+const building = {
+    numberOfFloors: 4,
+    numberOfAptByFloor: {
+        firstFloor: 3,
+        secondFloor: 4,
+        thirdFloor: 9,
+        fourthFloor: 2,
+    },
+    nameOfTenants: ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent:  {
+        sarah: [3, 990],
+        dan:  [4, 1000],
+        david: [1, 500],
+    },
+}
+
+// Console.log the number of floors in the building.
+// console.log(building.numberOfFloors);
+
+// Console.log how many apartments are on the floors 1 and 3.
+// console.log(building.numberOfAptByFloor.firstFloor);
+// console.log(building.numberOfAptByFloor.thirdFloor);
+
+
+// Console.log the name of the second tenant and the number of rooms he has in his apartment.
+// let secondTenant = building.nameOfTenants[1];
+// console.log(secondTenant);
+// let numberOfRooms = building.numberOfRoomsAndRent[secondTenant.toLowerCase()][0];
+// console.log(numberOfRooms);
+
+
+// Check if the sum of Sarah’s and David’s rent is bigger than Dan’s rent. 
+// If it is, than increase Dan’s rent to 1200.
+// if(building.numberOfRoomsAndRent.sarah[1] + building.numberOfRoomsAndRent.david[1] > building.numberOfRoomsAndRent.dan[1]){
+//     building.numberOfRoomsAndRent.dan = 1200;
+//     console.log("increasing");
+//     console.log(building.numberOfRoomsAndRent.dan);
+// }
+// else {
+//     console.log("leave alone");
+// };
+
