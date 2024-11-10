9d6d3cbc80256cfb75be309f633b6a503b2c8ffd
DanielOlivo <vit.masterov@gmail.com>
2024-11-10 09:57:45 +0200

    week 3 day 4 authentication part 3

--- Week3/Day4/ExerciseXP/authentication.py
+++ Week3/Day4/ExerciseXP/authentication.py
@@ -1,8 +1,57 @@
-credentials = {
-    'user1' : 'password1',
-    'user2' : 'password2',
-    'user3' : 'password3'
-}
+import bcrypt
+import psycopg2
+import pyperclip 
+
+
+class DbManager:
+
+    def __init__(self, connection):
+        self.connection = connection
+
+    def show_all(self):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM credentials;")
+        return cursor.fetchall()
+
+    def is_valid(self, username, password):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT password FROM credentials WHERE username=%s;", (username,))
+        hash = cursor.fetchone()
+        if hash is None:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), hash[0].encode('utf-8'))
+        
+
+    def add_user(self, username, password):
+        bytes = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hash = bcrypt.hashpw(bytes, salt)
+         
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO credentials(username, password) VALUES (%s, %s)", (username, hash.decode('utf-8')))
+        self.connection.commit()
+        print('user added')
+
+
+    def delete_user(self, username):
+        cursor = self.connection.cursor()
+        cursor.execute("DELETE FROM credentials WHERE username=%s;",(username,))
+        self.connection.commit()
+        print("user deleted")
+
+connection = psycopg2.connect(
+    host='localhost',
+    user='daniel',
+    password='1234',
+    dbname='challenge_auth'
+)
+db = DbManager(connection)
+
+# credentials = {
+#     'user1' : 'password1',
+#     'user2' : 'password2',
+#     'user3' : 'password3'
+# }
 
 logged_in = {
     'user1' : False,
@@ -10,25 +59,28 @@ logged_in = {
     'user3' : False
 }
 def signup_window():
-    global credentials, logged_in
-    while True:
-        username = input("return or username: ")
-        if username == 'return':
-            return
-
-        password = input("password: ")
-
-        if username in credentials:
-            print("you can't use this password")
-            continue
-
-        credentials[username] = password
-        logged_in[username] = False
+    # global credentials, logged_in
+    global db, logged_in
+    # while True:
+    username = input("return or username: ")
+    if username == 'return':
         return
+
+    password = input("password: ")
+
+    # if username in credentials:
+    #     print("you can't use this username")
+    #     continue
+
+    # credentials[username] = password
+    db.add_user(username, password)
+    logged_in[username] = False
+    return
     
 
 def login_window():
-    global credentials, logged_in
+    # global credentials, logged_in
+    global db, logged_in
     user_input = input("exit, signup or your login: ")
 
     if user_input == 'exit':
@@ -40,7 +92,8 @@ def login_window():
     else:
         password = input("password: ")
 
-        if not user_input in credentials or password != credentials[user_input]:
+        # if not user_input in credentials or password != credentials[user_input]:
+        if not db.is_valid(user_input, password):
             print("login or password not match")
             return None
             
@@ -70,7 +123,10 @@ def main():
             user_window(user)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    finally:
+        connection.close()
 
 
     
