8d726f09c88a2c380cf18ca0060cab87f33ac491
DanielOlivo <vit.masterov@gmail.com>
2024-11-10 18:07:01 +0200

    week 4 day 2 exercise gold

---
+++ Week4/Day2/ExerciseXP/exs_gold.js
@@ -0,0 +1,40 @@
+// exercise 1: divisible by three
+let numbers = [123, 8409, 100053, 333333333, 7]
+// for(number of numbers){
+//     console.log(`${number} is even: ${number % 2 == 0}`);
+// }
+
+// exercise 2: attendance
+let guestList = {
+    randy: "Germany",
+    karla: "France",
+    wendy: "Japan",
+    norman: "England",
+    sam: "Argentina"
+}
+
+// username = prompt("your name? ")
+// if(username in guestList){
+//     console.log(`Hi! I'm ${username}, and I'm from ${guestList[username]}.`);
+// }
+// else {
+//     console.log("Hi! I'm a guest.");
+// }
+
+
+// exercise 3: playing with numbers
+let age = [20,5,12,43,98,55];
+// 1. Console.log the sum of all the numbers in the age array.
+// let sum = 0;
+// for(a of age){
+//     sum += a;
+// }
+// console.log(`sum = ${sum}`);
+// 2. Console.log the highest age in the array.
+// let max = age[0]
+// for(i = 1; i < age.length; i++){
+//     if(age[i] > max){
+//         max = age[i];
+//     }
+// }
+// console.log(`max = ${max}`);
