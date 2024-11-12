4eabdab3ef207cea0b73301eb478e7abbce22b83
DanielOlivo <vit.masterov@gmail.com>
2024-11-12 14:03:44 +0200

    week 4 day 4 exs_gold shopping list

--- Week4/Day4/ExerciseXP/exs_gold.js
+++ Week4/Day4/ExerciseXP/exs_gold.js
@@ -24,3 +24,42 @@ document.querySelector('form input').onclick = function(e){
         }
     }
 }
+
+
+
+// Create an empty array. For example: let shoppingList=[].
+// Create a form containing : a text input field and an “AddItem” button. Add this form to the DOM.
+// Type what you need to buy in the text input field, then add the new item to the array as soon as you click on 
+// the “AddItem” button (Hint: create a function named addItem()).
+// Add a “ClearAll” button to the DOM, that when clicked on, should call the clearAll() function (see below).
+// Create a function named clearAll() that should clear out all the items in the shopping list.
+let shoppingList = [];
+
+const root = document.getElementById('root');
+const form = document.createElement('form');
+root.appendChild(form);
+
+const input = document.createElement('input');
+input.setAttribute('type', 'text');
+form.appendChild(input);
+
+const addBtn = document.createElement('button');
+addBtn.innerText = 'add';
+form.appendChild(addBtn);
+addBtn.onclick = function(e){
+    e.preventDefault();
+    if(input.value.trim().length > 0){
+        shoppingList.push(input.value.trim());
+        input.value = '';
+        console.log(`adding item: the list is ${shoppingList}`);
+    }
+}
+
+const clearBtn = document.createElement('button');
+clearBtn.innerText = 'clear';
+form.appendChild(clearBtn);
+clearBtn.onclick = function(e){
+    e.preventDefault();
+    shoppingList = [];
+    console.log('cleared');
+}
