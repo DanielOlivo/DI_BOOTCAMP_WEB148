00a62a303db36483980c61ee5889a12ae65b7ba0
DanielOlivo <vit.masterov@gmail.com>
2024-11-12 13:40:54 +0200

    week 4 day 4 exs_gold delete color

--- Week4/Day4/ExerciseXP/exs_gold.js
+++ Week4/Day4/ExerciseXP/exs_gold.js
@@ -7,3 +7,20 @@ document.getElementById('genres').onchange = function(e){
 // Add an additional option to the select tag:
 // <option value="classic">Classic</option>
 // The newly added option should be selected by default.
+
+
+
+// Add a click event listener to the <input type="button">. 
+// When clicked on, it should call a function named : removecolor() that removes 
+// the selected color from the dropdown list.
+document.querySelector('form input').onclick = function(e){
+    e.preventDefault(); 
+    const select = document.getElementById('colorSelect');
+    for(option of select.childNodes){
+        // console.log(`${select.value} vs ${option.value}`);
+        if(option.innerText == select.value){
+            option.remove();
+            return;
+        }
+    }
+}
