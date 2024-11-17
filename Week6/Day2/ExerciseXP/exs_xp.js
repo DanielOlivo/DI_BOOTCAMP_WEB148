d7a8878a08a3d1ea9eff92f2410d50782a897b74
DanielOlivo <vit.masterov@gmail.com>
2024-11-17 13:09:47 +0200

    week 6 day 2 exs_xp 2

--- Week6/Day2/ExerciseXP/exs_xp.js
+++ Week6/Day2/ExerciseXP/exs_xp.js
@@ -10,3 +10,26 @@ async function exercise1(){
         console.log(err);
     }
 }
+// exercise1()
+
+// exercise 2: giphy api
+async function exercise2(){
+    try{
+        let url = "https://api.giphy.com/v1/gifs/search?"
+        const params = {
+            q: 'sun',
+            api_key : "hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My",
+            offset : 2,
+            limit : 10
+        }
+        const line = Object.entries(params).map(([name, value]) => String(name) + '=' + String(value)).join('&&');
+        url += line;
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+// exercise2();
