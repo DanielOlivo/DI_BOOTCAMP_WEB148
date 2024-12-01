71f5b2b78c73f15755c2bef4b28e24f9cabcdc20
DanielOlivo <vit.masterov@gmail.com>
2024-12-01 18:04:04 +0200

    week 5 day 1 exs xp

--- Week5/Day1/ExerciseXP/exs_xp.js
+++ Week5/Day1/ExerciseXP/exs_xp.js
@@ -50,27 +50,112 @@ funcFour()
 funcFive() // 'hello'
 
 //#4
-let a = 1;
-function funcSix() {
-    let a = "test";
-    alert(`inside the funcSix function ${a}`);
-}
+// let a = 1;
+// function funcSix() {
+//     let a = "test";
+//     alert(`inside the funcSix function ${a}`);
+// }
 
 
 // #4.1 - run in the console:
-funcSix() // ANSWER: test, because the function has its own scope
+// funcSix() // ANSWER: test, because the function has its own scope
 // #4.2 What will happen if the variable is declared 
 // with const instead of let ?
 
 //#5
-let a = 2;
-if (true) {
-    let a = 5;
-    alert(`in the if block ${a}`);
-}
-alert(`outside of the if block ${a}`);
+// let a = 2;
+// if (true) {
+//     let a = 5;
+//     alert(`in the if block ${a}`);
+// }
+// alert(`outside of the if block ${a}`);
 
 // #5.1 - run the code in the console
 // #5.2 What will happen if the variable is declared 
 // with const instead of let ?
 // ANSWER: nothing, because they're in both scopes and unchanged
+
+
+// exercise 2: ternary operator
+const winBattle = () => {
+    return true;
+}
+const experiencePoints = winBattle() ? 10 : 1;
+
+
+// exercise 3: is it a string?
+const isString = (arg) => typeof arg === 'string';
+console.log(isString('hello')); 
+console.log(isString([1, 2, 4, 0]));
+
+
+// exercise 4: find the sum
+const sum = (a,b) => a + b;
+
+
+// exercise 5: kg and grams
+
+function toGrams1(kg) { // function declaration
+    return kg * 1000;
+}
+// console.log(toGrams1(1));
+
+const toGrams2 = function(kg) { // funciton expression
+    return kg * 1000;
+}
+// console.log(toGrams2(1));
+
+// expression starts as variable
+const toGrams3 = (kg) => kg * 1000;
+// console.log(toGrams3(1));
+
+
+// exercise 6: fortune teller
+(function fortuneTeller(job, geogpaphic, partner, numOfChildren){
+    const p = document.createElement('p')
+    p.appendChild(document.createTextNode(`You will be a ${job} in ${geogpaphic}, and married to ${partner} with ${numOfChildren} kids.`))
+    document.getElementById('exs6').appendChild(p);
+})('CEO', 'USA', 'Annabel', 10);
+
+
+// exercise 7: welcome
+(function (name){
+    const div = document.createElement('div');
+    div.appendChild(document.createTextNode(name));
+    document.getElementsByTagName('nav')[0].appendChild(div);
+})('Vitalii')
+
+
+// exercise 8: juice bar
+function makeJuice(size){
+    function addIngredients(fst, snd, thrd) {
+        const p = document.createElement('p');
+        p.appendChild(document.createTextNode(`The client wants a ${size} juice, containing ${fst}, ${snd}, ${thrd}.`));
+        document.getElementById('juice').appendChild(p);
+    }
+    addIngredients('one', 'two', 'three');
+}
+makeJuice('large');
+
+
+function makeJuice2(size){
+    let ingredients = []
+    function addIngredients(fst, snd, thrd){
+        ingredients.push(fst);
+        ingredients.push(snd);
+        ingredients.push(thrd);
+    }
+
+    function displayJuice(){
+        const p = document.createElement('p');
+        p.appendChild(document.createTextNode(`The client wants a ${size} juice, containing ${ingredients.join(', ')}.`));
+        document.getElementById('juice2').appendChild(p);
+    }
+
+    return [addIngredients, displayJuice];
+}
+
+const [fn1, fn2] = makeJuice2('large');
+fn1('fst', 'snd', 'thrd');
+fn1('fourth', 'fifth', 'sixth');
+fn2();
